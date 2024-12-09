01a8a0e82fa21bd8d6924ae291ec534885b52647
SebastienMorris <sebw.morris@gmail.com>
2024-12-09 15:25:15 +0100

    add enemy pattern structure

---
+++ Assets/Scripts/ScriptableObjects/EnemyPattern.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyPattern", menuName = "Scriptable Objects/EnemyPattern")]
+public class EnemyPattern : ScriptableObject
+{
+    public List<>
+}
