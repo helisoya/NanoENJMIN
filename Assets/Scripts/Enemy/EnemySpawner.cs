ef9e4155c68d795c369eba83f01d04b9e999ebde
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-12-10 15:07:46 +0100

    Fix enemy spawning in (0, 0)

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -23,7 +23,7 @@ public class EnemySpawner : MonoBehaviour
         int enemiesSpawned = 0;
         while (enemiesSpawned < pattern.enemies.Count)
         {
-            CreateEnemy(pattern.enemies[enemiesSpawned], _spline.Spline.EvaluatePosition(0f), Quaternion.LookRotation(-transform.right));
+            CreateEnemy(pattern.enemies[enemiesSpawned], (Vector3) _spline.Spline.EvaluatePosition(0f) + transform.position, Quaternion.LookRotation(-transform.right));
             enemiesSpawned++;
             yield return new WaitForSeconds(pattern.spawnRate);
         }
