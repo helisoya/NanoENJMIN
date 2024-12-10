48f358f90e9bdec4b4ea0d201d84162d2f2cdf85
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2024-12-10 14:54:09 +0100

    Change player projectile destroy x threshold

--- Assets/Scripts/Player/PlayerProjectile.cs
+++ Assets/Scripts/Player/PlayerProjectile.cs
@@ -54,6 +54,6 @@ public class PlayerProjectile : MonoBehaviour
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
 
-        if (transform.position.x >= 30) Destroy(gameObject);
+        if (transform.position.x >= 21.2f) Destroy(gameObject);
     }
 }
